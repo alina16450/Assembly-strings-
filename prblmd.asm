ee4798e08b22840b0f9b4495e1ea44d856b01c6d
Buliga Alina <161938026+alina16450@users.noreply.github.com>
2025-02-08 16:58:47 +0200

    Update prblmd.asm

--- prblmd.asm
+++ prblmd.asm
@@ -19,40 +19,40 @@ segment data use32 class=data
 segment code use32 class=code
     start:
         ; ...
-        mov ecx, lens
-        mov esi, 0
-        mov edi, 0
+        mov ecx, lens               ;loop counter
+        mov esi, 0                  ;index for S
+        mov edi, 0                  ;index for D
         
         repeat:
-            mov bx, 0
-            mov dl, 0
-            mov dh, 0
+            mov bx, 0               ;used for counting the 1's in the quadword in our loop
+            mov dl, 0               ;used for counting the number of bits that we are looping through
+            mov dh, 0               ;used for making sure we check both doublewords in the quad
             mov eax, dword[S+esi]
-            shifting:   
+            shifting:               
                 clc
                 inc dl
-                shl eax, 1
-                jc addition
-                cmp dl, 32
-                jnz shifting
-                jz final
+                shl eax, 1          ;we shift the bits, each time placing the leftmost bit in our carry flag
+                jc addition         ;if carry flag is set off, we have a 1 and it jumps to our label that handles the count
+                cmp dl, 32          ;if it is not a 1, we check if we still need to loop through the doubleword
+                jnz shifting        ;loops back to the next shift
+                jz next             ;exits the loop and jumps to a label that handles moving to next doubleword
                 
-                addition:
+                addition:           ;when bit is 1, we increment our counter, and check if we still need to loop through the doubleword
                     inc bx
                     cmp dl, 32
                     jnz shifting
-                    jz final
-            final:
-                inc dh
-                cmp dh, 2
-                jnz next
+                    jz next         ;if we do not need to loop, we jump to next step
+            next:
+                inc dh              ;if we are done with one doubleword, we check if the quad still has a half that has not been iterated through
+                cmp dh, 2           
+                jnz final           ;if we iterated through the entire quad, we move on to the next quad in the string
                 
-                test bx, 01b
-                jnz odd
-                add esi, 4
+                test bx, 01b        ;we check if our counter is odd
+                jnz odd             
+                add esi, 4          ;if it is not, we just increment our index for S and loop again
                 loop repeat
                 
-            next:
+            final:                  ;code segment that handles moving to the second part of the doubleword 
                 add esi, 4
                 mov eax, dword[S+esi]
                 mov dl, 0
@@ -60,11 +60,11 @@ segment code use32 class=code
                 
                 
             odd:
-                mov edx, dword[S+esi-4]
-                mov eax, dword[S+esi]
+                mov edx, dword[S+esi-4] ;in case our number needs to be added, we add both parts of it to D making sure to use the unmodified version
+                mov eax, dword[S+esi]   
                 mov [D+edi], edx
                 mov [D+edi+4], eax
-                add esi, 4
+                add esi, 4              ;increment both indexes and loop 
                 add edi, 8
                 loop repeat
                 
